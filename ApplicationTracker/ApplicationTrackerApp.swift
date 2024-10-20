78e7e5659134b06b3500bba0c748d45ff4ff423d
TheodoreUtomo <theodoreutomo@gmail.com>
2024-10-20 14:30:27 -0400

    Initial Commit

---
+++ ApplicationTracker/ApplicationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationTrackerApp.swift
+//  ApplicationTracker
+//
+//  Created by Theodore Utomo on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApplicationTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApplicationTracker
+//
+//  Created by Theodore Utomo on 10/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
