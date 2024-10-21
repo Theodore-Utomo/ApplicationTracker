3d5c4d219953b8ae1ceadcd164cebaacba39965e
TheodoreUtomo <theodoreutomo@gmail.com>
2024-10-20 22:17:52 -0400

    Basic UI and use of swift data

---
+++ ApplicationTracker/Application.swift
@@ -0,0 +1,8 @@
+//
+//  Application.swift
+//  ApplicationTracker
+//
+//  Created by Theodore Utomo on 10/20/24.
+//
+
+import Foundation
---
+++ SortedApplicationList.swift
@@ -0,0 +1,18 @@
+//
+//  SortedApplicationList.swift
+//  ApplicationTracker
+//
+//  Created by Theodore Utomo on 10/20/24.
+//
+
+import SwiftUI
+
+struct SortedApplicationList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SortedApplicationList()
+}
